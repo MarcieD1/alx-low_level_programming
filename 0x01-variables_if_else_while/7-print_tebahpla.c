3836c49e35be08b0b903e6b440dca8fd15123994
MarcieD1 <marciatarisai@icloud.com>
2023-08-11 20:07:06 +0200

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'Check for number if it is positive or negative'
+  *Return: always 0
+  */
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
