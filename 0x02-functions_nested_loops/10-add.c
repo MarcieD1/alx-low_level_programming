14c6a2049d4a671b4a742f688874f98c43d8c428
MarcieD1 <marciatarisai@icloud.com>
2023-08-16 01:01:59 +0200

    a +b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* add - Adds two values
-* @n1: The first number to be computed
-* @n2: The second number to be computed 
+* add-Adds two values
+* @n1:The first number to be computed
+* @n2:The second number to be computed
 * Return: Integer value
 */
 
