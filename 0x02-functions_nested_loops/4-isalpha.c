fdefce55c5a290fa29b1ff91fc8b72a53e3a05c8
MarcieD1 <marciatarisai@icloud.com>
2023-08-15 23:01:06 +0200

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
 * _isalpha - Checks for alphabetic character
-* @c: The character to be checked 
+* @c: The character to be checked
 * Return: 1 for alphabetic character or 0 for anything else
 */
 
 int _isalpha(int c)
 {
 
-if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+if ((c >= 65 && c <= 97) || ( c >= 97 && c <= 122))
 {
 return (1);
 }
