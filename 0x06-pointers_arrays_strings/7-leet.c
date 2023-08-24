7761cb1714767b926e5eef923817045cb9939d4e
MarcieD1 <marciatarisai@icloud.com>
2023-08-24 22:04:02 +0200

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,22 @@
  */
 char *leet(char *str)
 {
- char leet_chars[] = "aAeEoOtTlL";
- char leet_replacements[] = "4433007711";
+char leet_chars[] = "aAeEoOtTlL";
+char leet_replacements[] = "4433007711";
 
- int i, j;
+int i, j;
 
- for (i = 0; str[i] != '\0'; i++)
- {
- for (j = 0; leet_chars[j] != '\0'; j++)
- {
- if (str[i] == leet_chars[j])
- {
- str[i] = leet_replacements[j];
- break;
- }
- }
- }
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet_chars[j] != '\0'; j++)
+{
+if (str[i] == leet_chars[j])
+{
+str[i] = leet_replacements[j];
+break;
+}
+}
+}
 
-return str;
+return (str);
 }
