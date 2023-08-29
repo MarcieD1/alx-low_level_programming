9143c1037f8a8a2a4fe4fbc5742fe58ac7e98c65
MarcieD1 <marciatarisai@icloud.com>
2023-08-29 14:42:25 +0200

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,17 +2,17 @@
 #include <stddef.h>
 
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
- * Return: Pointer to the beginning of the located substring, or
- *         NULL if the substring is not found
- */
+* _strstr - Entry point
+* @haystack: input
+* @needle: input
+* Return: Pointer to the beginning of the located sub string, or
+* NULL if the sub string is not found
+*/
 char *_strstr(char *haystack, char *needle)
 {
 for (; *haystack != '\0'; haystack++)
 {
-char *l = haystack; 
+char *l = haystack;
 char *p = needle;
 
 while (*l == *p && *p != '\0')
